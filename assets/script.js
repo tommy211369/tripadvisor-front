f96010df0b127a6b1580d361d7519ed5b1419562
tommy211369 <tommy.thongrasamy@gmail.com>
2021-07-17 11:51:23 +0200

    tripadvisor front finish

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,53 @@
-let btnConnect = document.querySelector("#btn-connect");
-let btnClose = document.querySelector("#btn-close");
-let submitForm = document.querySelector("#form-contact");
+const $ = document;
 
-btnClose.addEventListener("click", () => {
-  document.body.style.overflowY = "initial";
-  document.querySelector("#modal").style.display = "none";
-  document.querySelector("#modal").classList.remove("show");
-});
+$.addEventListener("DOMContentLoaded", () => {
+  const form = $.querySelector("#form-contact");
+  const btnConnect = $.querySelector("#btn-connect");
+  const btnClose = $.querySelector("#btn-close");
 
-btnConnect.addEventListener("click", () => {
-  document.body.style.overflowY = "hidden";
-  document.querySelector("#modal").style.display = "block";
-  document.querySelector("#modal").classList.add("show");
-});
+  btnConnect.addEventListener("click", () => {
+    $.body.style.overflowY = "hidden";
+    $.querySelector("#modal").style.display = "block";
+    $.querySelector("#modal").classList.add("show");
+  });
 
-submitForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  location.reload();
+  btnClose.addEventListener("click", () => {
+    $.body.style.overflowY = "initial";
+    $.querySelector("#modal").style.display = "none";
+    $.querySelector("#modal").classList.remove("show");
+  });
+
+  // Fonction pour vider les champs du formulaire :
+  const cleanForm = () => {
+    form.reset();
+  };
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = {
+      firstName: $.querySelector("#first-name").value,
+      lastName: $.querySelector("#last-name").value,
+      email: $.querySelector("#email").value,
+      message: $.querySelector("#message").value,
+    };
+
+    console.log(data);
+
+    try {
+      const response = await axios.post("http://localhost:3000", data);
+      console.log(response);
+      if (response.status === 200) {
+        alert("Votre formulaire a bien été envoyé");
+        cleanForm();
+      }
+    } catch (e) {
+      if (e.response.data.error === "Missing parameters") {
+        alert("Veuillez remplir tous les champs du formulaire");
+      } else {
+        alert("Une erreur est survenue");
+        cleanForm();
+      }
+    }
+  });
 });
